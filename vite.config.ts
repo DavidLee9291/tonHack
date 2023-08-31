093d0ee1fe495aa9c02da4d2dcb9c2fd7af2e65e
auagho.o <auagho.o@gmail.com>
2023-08-31 16:31:14 +0900

    plz

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: 'https://davidlee9291.github.io/ton-testnet.github.io/',
+  base: 'https://davidlee9291.github.io/tonHack/',
 });
