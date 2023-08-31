a41d2d090db5af27dbc3185a8a23b4d275529ad1
kidd9291@gmail.com <kidd113@naver.com>
2023-08-31 15:16:05 +0900

    build 파일

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import React, { StrictMode } from "react";
-import { render } from "react-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.scss";
-import "./patch-local-storage-for-github-pages";
-import eruda from "eruda";
+import "./index.css";
+// this manifest is used temporarily for development purposes
+const manifestUrl =
+  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
-eruda.init();
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById("root") as HTMLElement
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </TonConnectUIProvider>
 );
